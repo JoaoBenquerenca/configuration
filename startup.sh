3576113d80088c01b3ca770937af3def1afb78da
JBenquerenca <37444053+JoaoBenquerenca@users.noreply.github.com>
2023-06-19 19:24:37 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,6 @@ INTERNAL_MONITOR="eDP-1"
 xrandr | grep $INTERNAL_MONITOR | grep "connected"
 if [ $? -eq 0 ]; then
   exec xrandr --output $INTERNAL_MONITOR --off
-  echo "hello world"
 fi
   
 # turn keyboard backlight on
