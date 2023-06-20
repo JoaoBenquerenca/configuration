21836bac4386895af68f3b4f17cb4cb58ef9c6b5
JBenquerenca <37444053+JoaoBenquerenca@users.noreply.github.com>
2023-06-20 18:01:10 +0100

    Update set_terminal_theme.sh

--- set_terminal_theme.sh
+++ set_terminal_theme.sh
@@ -4,12 +4,7 @@ cd "$HOME/src"
 git clone https://github.com/Gogh-Co/Gogh.git gogh
 cd gogh
 
-# necessary in the Gnome terminal on ubuntu
 export TERMINAL=gnome-terminal
-
-# necessary in the Alacritty terminal
-pip install -r requirements.txt
-export TERMINAL=alacritty
 
 # Enter theme installs dir
 cd installs
