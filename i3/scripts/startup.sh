4330f2df83c24b8503fb91f3ca65d9497ad3eddb
JBenquerenca <37444053+JoaoBenquerenca@users.noreply.github.com>
2023-06-20 18:02:30 +0100

    Update startup.sh

--- i3/scripts/startup.sh
+++ i3/scripts/startup.sh
@@ -4,9 +4,9 @@ xset led on
 EXTERNAL_MONITOR="DP-3"
 INTERNAL_MONITOR="eDP-1"
 
+# turn off laptop monitor
 xrandr | grep $INTERNAL_MONITOR | grep "connected"
 if [ $? -eq 0 ]; then
   exec xrandr --output $INTERNAL_MONITOR --off
-  echo "hello world"
 fi
   
