1ea4d28d1b0e270c5f6cf2231dda6f2a95e02b1b
JoaoBenquerenca <jbenquerenca9@gmail.com>
2023-06-25 19:21:20 +0100

    some fixes

--- i3/scripts/startup.sh
+++ i3/scripts/startup.sh
@@ -5,8 +5,8 @@ EXTERNAL_MONITOR="DP-3"
 INTERNAL_MONITOR="eDP-1"
 
 # turn off laptop monitor
-xrandr | grep $INTERNAL_MONITOR | grep "connected"
-if [ $? -eq 0 ]; then
+xrandr | grep $EXTERNAL_MONITOR | grep "disconnected"
+if [ $? -ne 0 ]; then
   exec xrandr --output $INTERNAL_MONITOR --off
 fi
   
